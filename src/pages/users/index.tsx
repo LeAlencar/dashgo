6ffcf66d6ad669673c1d0566af00758bb0f00d10
Leandro Alencar <zzleandrobritozz@gmail.com>
2022-06-09 20:25:52 -0300

    feat: add axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -7,11 +7,11 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 import { useQuery } from 'react-query'
+import { api } from "../../services/api";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json()
+  const { data, isLoading, isFetching, error } = useQuery('users', async () => {
+    const { data } = await api.get('users')
 
 
     const users =  data.users.map(user=> {
@@ -48,7 +48,12 @@ export default function UserList() {
 
         <Box flex="1" bg="gray.800" p="8" borderRadius={8}>
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+
+              { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4"/>}
+
+            </Heading>
             <Link passHref href="/users/create">
               <Button as="a" leftIcon={<Icon as={RiAddLine} fontSize="20" />} size="sm" fontSize="sm" colorScheme="pink">Criar novo usuário</Button>
             </Link>
